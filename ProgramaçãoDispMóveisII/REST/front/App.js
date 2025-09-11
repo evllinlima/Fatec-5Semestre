2ca411c985d101785e399176263f4370d9f406a5
Evellin Lima <evellinlima01@gmail.com>
2025-09-10 22:06:36 -0300

    Aprimora a estrutura do servidor e adiciona novas rotas para manipulação de dados, incluindo métodos GET, POST, DELETE e PUT. Atualiza o front-end para integrar as novas funcionalidades e corrige erros de digitação nos endpoints.

--- ProgramaçãoDispMóveisII/REST/front/App.js
+++ ProgramaçãoDispMóveisII/REST/front/App.js
@@ -1,23 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
-  //ip // atlas
-  const url = 'http://192.168.50.91:3000'; // verificar o ip com o comando ifconfig no terminal
-  // função para exibir os dados
-  const ExibirDados = (url) => {
-    fetch(url)
+  // ip // atlas
+  //const url ='http://localhost:3000'; 
+  const url = 'http://192.168.1.248:3000';
+  //const url ='http://10.0.2.2:3000'; 
+
+  // useEffect(() => { }, [])
+
+  // exebir o dados na tela
+  const ExibirDados = (urlX) => {
+    fetch(urlX)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-      })
+      }).catch((error) => {
+        console.error('Erro:', error);
+      });
   }
 
+  // inserir
+  const inserirUsuario = (urlX) => {
+    fetch(urlX + '/inserir', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: 'Maria'
 
-return (
+      }),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      }
+    }).then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+      }).catch((error) => {
+        console.error('Erro:', error);
+      });
+  }
+
+  // DELETAR
+  const deletarId = (urlX) => {
+    fetch(urlX + '/deletar/68b8d1d05c80b4878f82ea47', {
+      method: 'DELETE'
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+      }).catch((error) => {
+        console.error('Erro:', error);
+      });
+  }
+
+  // Alterar
+  const updateDados = (urlX) => {
+    fetch(urlX + '/alterar/??', {
+      method: 'PUT',
+      body: JSON.stringify({
+        name: 'TESTE'
+      }),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      }
+    }).then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
+  return (
     <View style={styles.container}>
-      <Button title="Exibir Dados" onPress={() => ExibirDados(url)} />
-      <StatusBar style="auto" />
+      <Button
+        title='Exibir Dados' onPress={() => ExibirDados(url)} />
+      <Button
+        title='Inserir Dados' onPress={() => inserirUsuario(url)} />
+
+      <Button title='Deletar ID' onPress={() => deletarId(url)} />
+
+      <Button title='Alterar Dados' onPress={() => updateDados(url)} />
     </View>
   );
 }
